db21fc21e07d7c3a8207557d872b7db7212179e9
Jakub Wilk <wilkjakub64@gmail.com>
2022-10-17 16:21:35 +0200

    Update 21.py

--- 02-TypesAndVariables/21.py
+++ 02-TypesAndVariables/21.py
@@ -1,2 +1,3 @@
-while True: __import__("random").seed(__import__("os").getpid()), __import__("sys").exit("True") if int(input("Enter a number:\n> ")) == __import__("random").randint(1, 6) else print("Wrong, try again")
+# while True: __import__("random").seed(__import__("os").getpid()), __import__("sys").exit("True") if int(input("Enter a number:\n> ")) == __import__("random").randint(1, 6) else print("Wrong, try again")
+(x:=__import__("random").randint(1,6),y:=lambda:(__import__("sys").exit("True") if x==int(input("Enter your guess:\n> ")) else print("False"),y()),y())
 # One line, without semicolons :)
